81611b170fec2b267b5f53ab2926e6375f243504
jimmyw32234 <jim.w32234@gmail.com>
2019-10-23 22:21:56 +0000

    first commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
 
+    $("#button").click(function(){
+        let input = $(".input").val();
+
+        let res = input[0] + "ay";
+        let restwo = input - input[0];
+        let final = restwo + res;
+        $(".output").html(res);
+    });
+
+    console.log(input)
 
 
-  
-  
-  
-  
+
+
+
+
 });
